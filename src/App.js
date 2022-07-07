50bc4024425b64df013b99750bf9ee2e5917b8bd
thais <thaisviana081@outlook.com>
2022-07-07 19:32:19 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Search from './components/search';
+import Category from './components/category';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path="/" component={ Search } />
-      </BrowserRouter>
+      <>
+        <h1>Home</h1>
+        <Category />
+        <BrowserRouter>
+          <Route exact path="/" component={ Search } />
+        </BrowserRouter>
+      </>
     );
   }
 }
